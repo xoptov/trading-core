b7238de006632244e5279d5b22dbd077a2a6d1d0
Maksim Khortov <xoptov@mail.ru>
2018-01-16 19:50:17 +0300

    Make some changes for Broker and create Channel for events.

---
+++ src/Channel.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Xoptov\TradingCore;
+
+use SplSubject;
+use SplObserver;
+use DeepCopy\DeepCopy;
+use SplDoublyLinkedList;
+
+class Channel implements SplSubject
+{
+	/** @var string */
+	private $event;
+
+	/** @var SplDoublyLinkedList */
+	private $subscribers;
+
+	/** @var mixed */
+	private $message;
+
+	/** @var DeepCopy */
+	private $copier;
+
+	/**
+	 * Channel constructor.
+	 *
+	 * @param string $event
+	 */
+	public function __construct($event)
+	{
+		$this->event = $event;
+		$this->copier = new DeepCopy();
+		$this->subscribers = new SplDoublyLinkedList();
+	}
+
+	/**
+	 * @param SplObserver $observer
+	 * @return bool
+	 */
+	public function attach(SplObserver $observer)
+	{
+		if ($this->subscribers->isEmpty()) {
+			$this->subscribers->push($observer);
+		}
+
+		foreach ($this->subscribers as $subscriber) {
+			if ($subscriber === $observer) {
+				return false;
+			}
+		}
+
+		$this->subscribers->push($observer);
+
+		return true;
+	}
+
+	/**
+	 * @param SplObserver $observer
+	 * @return bool
+	 */
+	public function detach(SplObserver $observer)
+	{
+		foreach ($this->subscribers as $key => $subscriber) {
+			if ($subscriber === $observer) {
+				$this->subscribers->offsetUnset($key);
+
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public function notify()
+	{
+		/** @var SplObserver $subscriber */
+		foreach ($this->subscribers as $subscriber) {
+			$subscriber->update($this);
+		}
+	}
+
+	/**
+	 * @param $message
+	 */
+	public function setMessage($message)
+	{
+		$this->message = $message;
+	}
+
+	/**
+	 * @return mixed|null
+	 */
+	public function getMessage()
+	{
+		if ($this->message) {
+			return $this->copier->copy($this->message);
+		}
+
+		return null;
+	}
+
+	public function flushMessage()
+	{
+		$this->message = null;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getEvent()
+	{
+		return $this->event;
+	}
+}
